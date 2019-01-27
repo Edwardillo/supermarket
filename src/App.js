980891460606e2c77a5d94b699a5f62b3dc29297
Edward Romero <edwardespinal16@gmail.com>
2019-01-26 20:35:10 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from 'antd/lib/button';
+import { Form, Card, InputNumber } from 'antd';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor (props) {
+        super(props);
+
+        const items = [
+            this.ItemObject('milk', 60, amount => amount * 40, amount => amount > 3),
+            this.ItemObject('egg', 5, amount => (5 * amount % 12) + (45 * (amount - (amount % 12))), (amount) => amount > 12),
+            this.ItemObject('bread', 3, amount => amount * (0.6), amount => amount > 2),
+        ];
+
+        this.state = {
+            items,
+            total: 0,
+        };
+    }
+
+    ItemObject = (label, price, specialPrice, specialPriceCondition) => {
+        return {
+            amount: 0,
+            total: function () {
+                return this.specialPriceCondition(this.amount) ? this.specialPrice(): this.price * this.amount;
+            },
+            label,
+            price,
+            specialPrice,
+            specialPriceCondition,
+        };
+    };
+
+    onChange = (value, index) => {
+        const {items} = this.state;
+        items[ index ].amount = value;
+
+        this.setState({items});
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const {items} = this.state;
+        const total = items.reduce((accumulator, element) => accumulator + element.total(), 0);
+
+        this.setState({total});
+    };
+
+    FormItem = (item, index) =>
+        <div>
+            <div>{ item.label }</div>
+            <InputNumber
+                min={ 0 }
+                defaultValue={ 0 }
+                onChange={ value => this.onChange(value, index) }
+            />
+        </div>;
+
+    render () {
+        const {items, total} = this.state;
+
+        return (
+            <div className="App">
+                    <Form onSubmit={ this.handleSubmit }>
+                        { items.map((item, index) => this.FormItem(item, index)) }
+                        <Button htmlType="submit">Submit</Button>
+                    </Form>
+                    {"Total:" + total}
+            </div>
+        );
+    }
 }
 
 export default App;
